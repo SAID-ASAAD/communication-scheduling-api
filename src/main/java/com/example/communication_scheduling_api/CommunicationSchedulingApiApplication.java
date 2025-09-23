527fe3c775eadb36aff686dced849ec1b8efebb7
SaidAsaad <saidyousef201513@gmail.com>
2025-09-22 22:54:16 -0300

    Initial commit

---
+++ src/main/java/com/example/communication_scheduling_api/CommunicationSchedulingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.communication_scheduling_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationSchedulingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationSchedulingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/communication_scheduling_api/CommunicationSchedulingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communication_scheduling_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationSchedulingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
